a90bc157907d71eb1d3c5ada5eba7c3fe48824fc
Sinitsyn Alekcander <sinitsyn.alekcander@gmail.com>
2021-12-02 21:10:44 +0300

    Add module.css for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import objectHeaderModulesCss from './Header.module.css';
 
 function Header() {
-    return <header className='appHeader'>
+    return <header className={objectHeaderModulesCss.appHeader}>
         <img src='https://yt3.ggpht.com/a/AGF-l79lzK-J9srTPskVDvzM9MaYvHZckUPAS4GNFA=s1920-mo-c-c0xffffffff-rj-k-no' />
     </header>
 }
