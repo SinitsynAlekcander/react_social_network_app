92a2705bd6a44fdb7aec693129a69a7eeba3a60f
Sinitsyn Alekcander <sinitsyn.alekcander@gmail.com>
2021-12-14 13:06:14 +0300

    Add NavLink in App.js

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,37 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import objectNavbarModuleCss from './Navbar.module.css';
 
 function Navbar() {
     return <nav className={objectNavbarModuleCss.appNav}>
-        <div className={objectNavbarModuleCss.item}>
-            <a href="/profile"> Profile </a>
+        <div>
+            <NavLink to="/profile" className={navData => navData.isActive ? objectNavbarModuleCss.active : objectNavbarModuleCss.item}>
+                Profile
+            </NavLink>
         </div>
-        <div className={objectNavbarModuleCss.item}>
-            <a href="/dialogs"> Messeges </a>
+
+        <div>
+            <NavLink to="/dialogs" className={navData => navData.isActive ? objectNavbarModuleCss.active : objectNavbarModuleCss.item}>
+                Messeges
+            </NavLink>
         </div>
-        <div className={objectNavbarModuleCss.item}>
-            <a href="/news">  News </a>
+
+        <div>
+            <NavLink to="/news" className={navData => navData.isActive ? objectNavbarModuleCss.active : objectNavbarModuleCss.item}>
+                News
+            </NavLink>
         </div>
-        <div className={objectNavbarModuleCss.item}>
-            <a href="/music"> Music </a>
+
+        <div>
+            <NavLink to="/music" className={navData => navData.isActive ? objectNavbarModuleCss.active : objectNavbarModuleCss.item}>
+                Music
+            </NavLink>
         </div>
-        <div className={objectNavbarModuleCss.item}>
-            <a href="/settings"> Settings </a>
+
+        <div>
+            <NavLink to="/settings" className={navData => navData.isActive ? objectNavbarModuleCss.active : objectNavbarModuleCss.item}>
+                Settings
+            </NavLink>
         </div>
     </nav>
 }
