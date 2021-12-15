ea2378c68a8be7c52ff71c86da2e31e08d15f69c
Sinitsyn Alekcander <sinitsyn.alekcander@gmail.com>
2021-12-15 14:20:40 +0300

    the beginning of registration for <Dialogs />

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,28 @@
 import React from "react"
-import objectDialogsModuleCss from './Dialogs.module.css';
+import style from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    Sasha
+                </div>
+                <div className={style.dialog}>
+                    Tatyana
+                </div>
+                <div className={style.dialog}>
+                    Demian
+                </div>
+                <div className={style.dialog}>
+                    Alisa
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How are you?</div>
+                <div className={style.message}>Yes</div>
+            </div>
         </div>
     )
 }
