f721dd8c40781e9965d99c26cff503a79e2591fc
SinitsynAlekcander <sinitsyn.alekcander@gmail.com>
2022-08-09 12:13:05 +0300

    Add component <Header/>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app-wraper">
+      <Header />
 
-      <header className="header">
-        <img src="https://avatars.mds.yandex.net/i?id=446734f9157fa66ecc70cdf6ee332f6f-5865789-images-thumbs&n=13&exp=1" />
-      </header>
 
       <nav className="nav">
         <div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="header">
+            <img src="https://avatars.mds.yandex.net/i?id=446734f9157fa66ecc70cdf6ee332f6f-5865789-images-thumbs&n=13&exp=1" />
+        </header>
+    )
+}
+
+export default Header;
