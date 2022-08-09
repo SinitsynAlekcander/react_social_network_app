77ebe62404895b6b091731317c0dbbff05c1e263
SinitsynAlekcander <sinitsyn.alekcander@gmail.com>
2022-08-09 12:35:21 +0300

    Add component <Profile/>

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="app-wraper">
       <Header />
       <Navbar />
-
-      <div className="main-conent">
-        <div>
-          <img src="https://hb.bizmrg.com/websiterfs/news/216431/62eff614cd7ce_582x388.jpg" />
-        </div>
-        <div>
-          Ava + description
-        </div>
-        <div>
-          My posts
-          <div>
-            New post
-          </div>
-        </div>
-        <div>
-          <div>
-            post 1
-          </div>
-          <div>
-            post 2
-          </div>
-        </div>
-      </div>
+      <Profile/>
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+function Profile() {
+    return (
+        <div className="main-conent">
+            <div>
+                <img src="https://hb.bizmrg.com/websiterfs/news/216431/62eff614cd7ce_582x388.jpg" />
+            </div>
+            <div>
+                Ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile;
