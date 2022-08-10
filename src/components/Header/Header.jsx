05937641eef8efe307550f3a70beb8ce229cdf1a
SinitsynAlekcander <sinitsyn.alekcander@gmail.com>
2022-08-10 21:00:12 +0300

    Refactring <Header/>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,9 @@ import style from './Header.module.css'
 function Header() {
     return (
         <header className={style.header}>
-            <img src="https://avatars.mds.yandex.net/i?id=446734f9157fa66ecc70cdf6ee332f6f-5865789-images-thumbs&n=13&exp=1" />
+            <div>
+                <img src="https://avatars.mds.yandex.net/i?id=446734f9157fa66ecc70cdf6ee332f6f-5865789-images-thumbs&n=13&exp=1" />
+            </div>
             <div className={style.text}>
                 Social Network
             </div>
