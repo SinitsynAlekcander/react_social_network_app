746231c6b66e72869596db1faa398d77fe81890c
SinitsynAlekcander <sinitsyn.alekcander@gmail.com>
2022-08-10 20:31:06 +0300

    Beginning design for <Dialogs/>

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,26 @@ import style from './Dialogs.module.css'
 function Dialogs() {
     return (
         <div className={style.dialogs}>
-            Dialogs
+            <div className={style.dialog_item}>
+                <div className={style.dialog + ' ' + style.active}>
+                    Demian
+                </div>
+                <div className={style.dialog}>
+                    Sasha
+                </div>
+            </div>
+
+            <div className={style.messages}>
+                <div className={style.message}>
+                    Hello
+                </div>
+                <div className={style.message}>
+                    World
+                </div>
+                <div className={style.message}>
+                    )))))
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,9 @@ function Header() {
     return (
         <header className={style.header}>
             <img src="https://avatars.mds.yandex.net/i?id=446734f9157fa66ecc70cdf6ee332f6f-5865789-images-thumbs&n=13&exp=1" />
-        Social Network
+            <div className={style.text}>
+                Social Network
+            </div>
         </header>
     )
 }
