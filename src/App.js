896948140cf978749e8a06f15a6e383101c6de31
SinitsynAlekcander <sinitsyn.alekcander@gmail.com>
2022-08-10 16:41:57 +0300

    Add ROUTES and add components <News/>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import News from "./components/News/News";
 import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
-    <div className="app_wraper">
-      <Header />
-      <Navbar />
-      <div className="app_wraper_content">
-        {/* <Profile/> */}
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app_wraper">
+        <Header />
+        <Navbar />
+        <div className="app_wraper_content">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ function Navbar() {
     return (
         <nav className={style.nav}>
             <div className={style.item}>
-                <a>Profile</a>
+                <a href='/profile'>Profile</a>
             </div>
             <div className={style.item}>
-                <a>Message</a>
+                <a href='/dialogs'>Dialogs</a>
             </div>
             <div className={style.item}>
-                <a>News</a>
+                <a href='/news'>News</a>
             </div>
             <div className={style.item}>
                 <a>Music</a>
