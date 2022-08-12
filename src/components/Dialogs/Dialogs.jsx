bcf50b69622f60cbf57cd8cbd89c0138a4c57f9f
SinitsynAlekcander <sinitsyn.alekcander@gmail.com>
2022-08-12 16:07:37 +0300

    Add .map for <Dialogs/>

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -10,7 +10,6 @@ function DialogItem(props) {
         </div>
     )
 }
-
 function Message(props) {
     return (
         <div className={style.message}>
@@ -18,7 +17,6 @@ function Message(props) {
         </div>
     )
 }
-
 function Dialogs() {
     let dialogsData = [
         { id: 1, name: "Damian" },
@@ -31,19 +29,17 @@ function Dialogs() {
         { id: 2, message: "World" },
         { id: 3, message: ")))" },
     ]
+    let dialogsElements = dialogsData
+        .map(dialog => <DialogItem name={dialog.name} id={dialog.id} />);
+    let mesagesElements = messagesData
+        .map(message => <Message message={message.message} />);
     return (
         <div className={style.dialogs}>
             <div className={style.dialog_item}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+                {dialogsElements}
             </div>
-
             <div className={style.messages}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
-                <Message message={messagesData[2].message} />
+                {mesagesElements}
             </div>
         </div>
     )
