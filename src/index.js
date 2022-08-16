d31e0fac13ff1a0ca7f941d7b8968afa594215e2
SinitsynAlekcander <sinitsyn.alekcander@gmail.com>
2022-08-16 16:22:07 +0300

    Deleting unnecessary comments

--- src/index.js
+++ src/index.js
@@ -5,22 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
 
-// let postsData = [
-//   { id: 1, message: "First post", likeCount: 10 },
-//   { id: 2, message: "Second post", likeCount: 12 }
-// ]
-// let dialogsData = [
-//   { id: 1, name: "Damian" },
-//   { id: 2, name: "Alisa" },
-//   { id: 3, name: "Sasha" },
-//   { id: 4, name: "Tanya" },
-// ]
-// let messagesData = [
-//   { id: 1, message: "Hello" },
-//   { id: 2, message: "World" },
-//   { id: 3, message: ")))" },
-// ]
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,19 +1,3 @@
-// let postsData = [
-//     { id: 1, message: "First post", likeCount: 10 },
-//     { id: 2, message: "Second post", likeCount: 12 }
-// ]
-// let dialogsData = [
-//     { id: 1, name: "Damian" },
-//     { id: 2, name: "Alisa" },
-//     { id: 3, name: "Sasha" },
-//     { id: 4, name: "Tanya" },
-// ]
-// let messagesData = [
-//     { id: 1, message: "Hello" },
-//     { id: 2, message: "World" },
-//     { id: 3, message: ")))" },
-// ]
-
 let state = {
     profilePage: {
         postsData: [
