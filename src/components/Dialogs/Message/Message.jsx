42a078dbbd16784ecbdac26aa3c71301233d4645
SinitsynAlekcander <sinitsyn.alekcander@gmail.com>
2022-08-17 13:08:28 +0300

    Add <textarea/> and <button/> for Message.jsx

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import style from './Message.module.css'
 
 function Message(props) {
+
+    let newMessageElement = React.createRef();
+    let addMessage = () => { alert(newMessageElement.current.value) };
+
     return (
         <div className={style.message}>
-            {props.message}
-        </div>
+            <div>
+                {props.message}
+            </div>
+            <div>
+                <textarea ref={newMessageElement}>Post</textarea>
+                <button onClick={addMessage}>Add message</button>
+            </div>
+        </div >
     )
 }
 
