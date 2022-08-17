6c4b27a279db8407c4487452b5ffe00cd71a1f76
SinitsynAlekcander <sinitsyn.alekcander@gmail.com>
2022-08-17 12:37:24 +0300

    Add onClick for button (addPost) [index.js]

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,23 +3,21 @@ import style from './MyPosts.module.css'
 import Post from './Post/Post';
 
 function MyPosts(props) {
-    // let postsData = [
-    //     { id: 1, message: "First post", likeCount: 10 },
-    //     { id: 2, message: "Second post", likeCount: 12 }
-    // ]
 
     let postsElements = props.postsData
         .map(post => <Post message={post.message} likeCount={post.likeCount} />);
+    let newPostElement = React.createRef();
+    let addPost = () => { alert(newPostElement.current.value) };
 
     return (
         <div className={style.description_block}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea>Post</textarea>
+                    <textarea ref={newPostElement}>Post</textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                     <button>Delete</button>
                 </div>
             </div>
